cc0e5e37c93366a374e2205ffa2811316ec0f089
Louis Hol <louis_hol@hotmail.com>
2015-05-09 14:46:37 +0200

    Routing voor sprint 2

--- app/js/app.js
+++ app/js/app.js
@@ -1,6 +1,30 @@
 require('angular/angular');
-var module = angular.module('app', []);
-var service = require('../games/services/GameService');
-var controller = require('../games/controllers/GameController')
-module.service('GameService', service);
-module.controller('GameController', controller);
+require('angular-route');
+var module = angular.module('app', ['ngRoute']);
+ var service = require('../games/services/GameService');
+ var controller = require('../games/controllers/GameController')
+ module.service('GameService', service);
+ module.controller('BeheerController', controller);
+
+module.config(['$routeProvider', function($routeProvider, $locationProvider) {
+  $routeProvider.
+  when('/beheer', {
+   	  templateUrl: "../beheer.html",
+      controller: "BeheerController",
+      controllerAs: "beheer"
+  }).
+  when('/authcallback', {
+       templateUrl: "../callbackurl.html",
+      controller: "CallbackController",
+      controllerAs: "callback"
+  })
+}]);
+
+
+
+module.controller('CallbackController', function($routeParams) {
+  var self = this;
+  self.message = $routeParams;
+});
+
+
