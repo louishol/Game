e4cd438347eb4580acccdc51c7d8fd5725d309c1
Louis Hol <louis_hol@hotmail.com>
2015-05-18 20:44:34 +0200

    Detailpagina

--- app/games/controllers/DetailsController.js
+++ app/games/controllers/DetailsController.js
@@ -23,5 +23,9 @@ var Game = function($scope, $routeParams, DetailsService)
         }, function(error) {
             $scope.tiles = "";
         });
+
+    $scope.removeTile = function(tile) {
+        $scope.tiles.splice($scope.tiles.indexOf(tile), 1);
+    };
 }
 module.exports = Game;
